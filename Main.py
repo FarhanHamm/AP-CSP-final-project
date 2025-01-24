ce55e1fb0a55ff9ae4bbae944c80c1c0bdccfc58
FarhanHamm <155015364+FarhanHamm@users.noreply.github.com>
2025-01-24 16:55:59 -0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,15 @@
+import random
+
+EasyList = list(range(1, 11))
+MediumList = list(range(1, 26))
+HardList = list(range(1, 51))
+
+
+while True:
+    print("Welcome to the random number guessing game! Please choose a difficulty:")
+    print("Easy \n Medium \n Hard")
+    break
+
+print("Welcome to the random number guessing game! Please choose a difficulty:")
+print("Easy \n Medium \n Hard")
+
